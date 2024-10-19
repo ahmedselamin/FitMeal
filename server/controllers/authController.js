c5dcf72aa2846b30449d2dde5144ab2ced7fcbb5
eddyiiz <eddyxsayf@gmail.com>
2024-10-19 11:38:38 +0200

    add login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -17,3 +17,24 @@ export const register = async (req, res) => {
         res.status(500).json({ message : error })
     }
 };
+
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if(!user){
+            res.status(401).json({ message : "Invalid credentials" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            res.status(401).json({ message : "Invalid credentials" })   
+        }
+        const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET, { expiresIn: '1h' })        
+        res.json({ token });
+
+    } catch (error) {
+        res.status(500).json({ message : error })
+    }
+}
