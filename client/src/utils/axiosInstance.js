49347eadef86cc59f581e521aa0ac4db7f6eea4c
eddyiiz <eddyxsayf@gmail.com>
2024-10-20 11:10:51 +0200

    handle unauthorized

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -15,3 +15,19 @@ axiosInstance.interceptors.request.use((config) => {
 
     return config;
 });
+
+//handle unauthorized
+axiosInstance.interceptors.response.use(
+    (response) => response,
+
+    (error) => {
+        if (error.response?.status == 401){
+            localStorage.removeItem('token');
+            window.location.href = "/";
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
