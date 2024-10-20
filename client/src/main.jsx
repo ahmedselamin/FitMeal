962b98ec1f80985612b9d77b46da2c502d704777
eddyiiz <eddyxsayf@gmail.com>
2024-10-20 11:51:51 +0200

    add routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import homePage from "./pages/homePage"
+import savedPage from "./pages/savedPage"
+import protectedRout from "./components/protectedRoute"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <homePage />
+  },
+  {
+    path: "/saved",
+    element:
+        <protectedRout>
+          <savedPage />
+        </protectedRout>        
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <RouterProvider router={router} />    
   </StrictMode>,
 )
