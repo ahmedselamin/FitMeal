c27a4126a35e30bacba7e2ac0983d61e21eef2c6
eddyiiz <eddyxsayf@gmail.com>
2024-10-20 19:51:55 +0200

    debug

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-import express from 'express'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url'; // This will help get the current file's path
 import cors from "cors";
 import mongoose from 'mongoose';
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/post.js";
 
@@ -20,9 +22,17 @@ mongoose.connect(DATABASE_URL)
   .then(() => console.log('DB connected'))
   .catch((err) => console.log('Error connecting to MongoDB:', err));
 
+// ES module replacement for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Static file serving for images
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-})
+    console.log(`Server running on port ${port}`);
+});
