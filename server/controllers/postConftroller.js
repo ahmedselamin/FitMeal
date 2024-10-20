fb89c25261e42e7f4b7ab03fef86b334fc6a5cf2
eddyiiz <eddyxsayf@gmail.com>
2024-10-20 09:16:31 +0200

    add delete

--- server/controllers/postConftroller.js
+++ server/controllers/postConftroller.js
@@ -55,3 +55,15 @@ router.put('/:id', async (req, res) => {
     res.status(500).json({ message: 'Error updating post' });
   }
 });
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedPost = await Post.findByIdAndDelete(req.params.id);
+    if (!deletedPost) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    res.json({ message: 'Post deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error deleting post' });
+  }
+});
