27a11c23ad2ad4befcc6e4d8750e897e169543ab
eddyiiz <eddyxsayf@gmail.com>
2024-10-20 09:21:31 +0200

    refactoring

--- server/controllers/postConftroller.js
+++ server/controllers/postConftroller.js
@@ -1,16 +1,30 @@
-import Post from "../models/post.js"
+import Post from '../models/Post.js';
+
+// Create a new post
+export const createPost = async (req, res) => {
+  const { title, description, ingredients, instructions, author } = req.body;
+
+  try {
+    const newPost = new Post({ title, description, ingredients, instructions, author });
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating post' });
+  }
+};
 
 // Get all posts
-router.get('/', async (req, res) => {
-    try {
-      const posts = await Post.find().populate('author', 'username'); 
-      res.json(posts);
-    } catch (err) {
-      res.status(500).json({ message: 'Error fetching posts' });
-    }
-});
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate('author', 'username');
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching posts' });
+  }
+};
 
-router.get('/:id', async (req, res) => {
+// Get a single post by ID
+export const getPostById = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id).populate('author', 'username');
     if (!post) {
@@ -20,23 +34,10 @@ router.get('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: 'Error fetching post' });
   }
-});
+};
 
-// Create a new post
-router.post('/', async (req, res) => {
-    const { title, description, ingredients, instructions, author } = req.body;
-  
-    try {
-      const newPost = new Post({ title, description, ingredients, instructions, author });
-      await newPost.save();
-      res.status(201).json(newPost);
-    } catch (err) {
-      res.status(500).json({ message: 'Error creating post' });
-    }
-});
-
-
-router.put('/:id', async (req, res) => {
+// Update a post by ID
+export const updatePostById = async (req, res) => {
   const { title, description, ingredients, instructions } = req.body;
 
   try {
@@ -54,9 +55,10 @@ router.put('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: 'Error updating post' });
   }
-});
+};
 
-router.delete('/:id', async (req, res) => {
+// Delete a post by ID
+export const deletePostById = async (req, res) => {
   try {
     const deletedPost = await Post.findByIdAndDelete(req.params.id);
     if (!deletedPost) {
@@ -66,4 +68,4 @@ router.delete('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: 'Error deleting post' });
   }
-});
+};
