54117235c861d6c2cc6e1459632353cebf7b0b13
eddyiiz <eddyxsayf@gmail.com>
2024-10-20 09:35:11 +0200

    auth routes

--- server/routes/post.js
+++ server/routes/post.js
@@ -6,13 +6,14 @@ import {
   updatePostById,
   deletePostById,
 } from '../controllers/postConftroller.js';
+import authMiddleware from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post('/', createPost);
+router.post('/', authMiddleware, createPost);
 router.get('/', getAllPosts);
 router.get('/:id', getPostById);
-router.put('/:id', updatePostById);
-router.delete('/:id', deletePostById);
+router.put('/:id', authMiddleware, updatePostById);
+router.delete('/:id', authMiddleware, deletePostById);
 
 export default router;
