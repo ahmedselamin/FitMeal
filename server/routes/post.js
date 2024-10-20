3986a54988cc4e0ec32ff3548bac23695fe2e94c
eddyiiz <eddyxsayf@gmail.com>
2024-10-20 10:12:40 +0200

    add upload middleware

--- server/routes/post.js
+++ server/routes/post.js
@@ -7,12 +7,13 @@ import {
   deletePostById,
 } from '../controllers/postConftroller.js';
 import authMiddleware from "../middleware/auth.js";
+import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.get('/', getAllPosts);
 router.get('/:id', getPostById);
-router.post('/', authMiddleware, createPost);
+router.post('/', authMiddleware, upload.single('image'), createPost);
 router.put('/:id', authMiddleware, updatePostById);
 router.delete('/:id', authMiddleware, deletePostById);
 
