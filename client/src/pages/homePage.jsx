b8e02a3984a11c8d66aae1dc0e522f1f0e313d64
eddyiiz <eddyxsayf@gmail.com>
2024-10-21 08:09:09 +0200

    handle submit

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -19,30 +19,37 @@ const HomePage = () => {
   const [open, setOpen] = useState(false);
   const [formData, setFormData] = useState({
     title: "",
-    image: "",
     ingredients: "",
     instructions: "",
+    image: null, 
     author: ""
   });
 
   const handleInputChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name] : e.target.value
+      [e.target.name]: e.target.value
     });
   };
   
-  const clearForm = () =>{
+  const handleImageChange = (e) => {
+    setFormData({
+      ...formData,
+      image: e.target.files[0] // Set the selected image file
+    });
+  };
+
+  const clearForm = () => {
     setFormData({
       title: "",
-      image: "",
+      image: null,
       ingredients: "",
       instructions: "",
       author: ""
     });
   };
 
-  const openDialog = () =>{
+  const openDialog = () => {
     setOpen(true);
   }
 
@@ -51,10 +58,32 @@ const HomePage = () => {
     clearForm();
   }
 
-  const handleFormSubmit = () => {
-    console.log("form")
-  }
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData();
+  data.append('title', formData.title);
+  data.append('ingredients', formData.ingredients);
+  data.append('instructions', formData.instructions);
   
+  // If there's an image, append it to the FormData
+  if (formData.image) {
+    data.append('image', formData.image);
+  }
+
+  try {
+    const response = await axiosInstance.post('/posts', data, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    closeDialog(); 
+    fetchPosts();
+    } catch (error) {
+      console.error('Error submitting post:', error);
+    }
+  };
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -82,7 +111,7 @@ const HomePage = () => {
             paddingY: '10px',
             transition: 'transform 0.3s ease',
             '&:hover': {
-                transform: 'scale(1.1)',
+              transform: 'scale(1.1)',
             },
           }} 
         >
@@ -136,24 +165,50 @@ const HomePage = () => {
         <DialogTitle sx={{ textAlign: "center" }}>Share A Recipe</DialogTitle>
         <DialogContent>
           <Box component="form" onSubmit={handleFormSubmit}>
-          <TextField
-           margin="dense"
-           name="title"
-           label="name"
-           type="text"
-           fullWidth
-           required
-           value={formData.title}
-           onChange={handleInputChange}                               
-          />
-          <DialogActions sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 2 }}>
-             <Button variant="outlined" onClick={closeDialog}>
-               Discard
-             </Button>
-             <Button variant="contained" type="submit" color="primary">
-               Confirm
-             </Button>                                
-           </DialogActions>                                           
+            <TextField
+              margin="dense"
+              name="title"
+              label="Name"
+              type="text"
+              fullWidth
+              required
+              value={formData.title}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="ingredients"
+              label="Ingredients"
+              type="text"
+              fullWidth
+              required
+              value={formData.ingredients}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="instructions"
+              label="Instructions"
+              type="text"
+              fullWidth
+              required
+              value={formData.instructions}
+              onChange={handleInputChange}
+            />
+            <input
+              accept="image/*"
+              type="file"
+              onChange={handleImageChange}
+              style={{ marginTop: '10px' }}
+            />
+            <DialogActions sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 2 }}>
+              <Button variant="outlined" onClick={closeDialog} sx={{ color: 'black', borderRadius: '20px' }}>
+                Discard
+              </Button>
+              <Button variant="contained" type="submit" sx={{ backgroundColor: '#005477', borderRadius: '20px' }}>
+                Confirm
+              </Button>
+            </DialogActions>
           </Box>
         </DialogContent>
       </Dialog>
