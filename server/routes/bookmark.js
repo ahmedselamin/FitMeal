bfa6c27d82786ecfa6bc569e9c84251970cdca92
eddyiiz <eddyxsayf@gmail.com>
2024-10-22 19:49:03 +0200

    remove bookmark

--- server/routes/bookmark.js
+++ server/routes/bookmark.js
@@ -21,3 +21,21 @@ export const addBookmark = async (req, res) => {
         return res.status(500).json({ message : error })
     }
 }
+
+export const removeBookmark = async (req, res) => {
+    const userId = req.User.Id;
+    const postId = req.params.id;
+
+    try {
+        const user = await findById(userId);
+
+        if(!user.bookmarks.includes(postId)){
+            return res.status(400).json({ message: "Post not bookmarked" })
+        }
+
+        user.bookmarks.filter(id => id.toString() !== postId)
+        return res.status(201).json({ message: "removed from bookmarks" })
+    } catch (error) {
+        return res.status(500).json({ message: error })
+    }
+}
