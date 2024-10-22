18936328e37b0c9c63ccc13ce7913916393e84cf
eddyiiz <eddyxsayf@gmail.com>
2024-10-22 13:25:58 +0200

    add bookmarks

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    bookMark: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }]
 })
 
 const User = mongoose.model("User", userSchema)
