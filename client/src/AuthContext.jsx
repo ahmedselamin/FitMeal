641647e4574a3bb63297cbe4532db987949d0f92
eddyiiz <eddyxsayf@gmail.com>
2024-10-22 05:59:26 +0200

    add register

--- client/src/AuthContext.jsx
+++ client/src/AuthContext.jsx
@@ -1,13 +1,26 @@
 import { createContext, useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
-import axiosInstance from './utils/axiosInstance'; // Adjust the path if necessary
+import axiosInstance from './utils/axiosInstance'; 
 
 // Create the AuthContext
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [username, setUsername] = useState('');
+
+  const register = async (formData) => {
+    try {
+        const response = await axiosInstance.post('/auth/register',{
+            username: formData.username,
+            password: formData.password,
+        });
+
+    } catch (error) {
+        console.error(error);        
+    }
+  }
 
   const login = async (formData) => {
     try {
@@ -28,9 +41,10 @@ export const AuthProvider = ({ children }) => {
       setUsername(user);
 
       return true; 
+      
     } catch (error) {
       console.error('Login error:', error);
-      
+
       return false; 
     }
   };
@@ -43,7 +57,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, username, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, username, register, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
